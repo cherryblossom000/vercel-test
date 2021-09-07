bedff82adcc39e7ebeb3f9a944cfdf8c37f7db12
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-09-07 14:01:22 +1000

    init

---
+++ src/hello.ts
@@ -0,0 +1,10 @@
+import 'node-fetch'
+import type {VercelRequest, VercelResponse} from '@vercel/node'
+
+export default async (
+  request: VercelRequest,
+  response: VercelResponse
+): Promise<void> => {
+  const {name = 'World'} = request.query
+  response.status(200).send(`Hello ${name}!`)
+}
